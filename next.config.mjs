dcf11ab8efb0638c229de568414128ce2066e216
hayuri1990 <hayuri1990@gmail.com>
2025-07-07 19:40:16 +0900

    feat: 카카오 기본 프로필 이미지 도메인 추가

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,8 @@ const nextConfig = {
   images: {
     domains: [
       'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
-      'k.kakaocdn.net',
+      'k.kakaocdn.net', // 카카오 프로필 이미지
+      'img1.kakaocdn.net', // 카카오 기본 프로필 이미지
       'example.com',
       'lh3.googleusercontent.com',
       'hips.hearstapps.com',
